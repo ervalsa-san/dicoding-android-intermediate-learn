57490e08f2e6cfaf97dd03cade45dc7592429850
ervalsa <ervalsa-san@gmail.com>
2022-10-22 02:33:01 +0800

    Setup User Preferences

--- 6-submission-aplikasi-story-app/StoryApp/app/src/main/java/com/ervalsa/storyapp/data/model/User.kt
+++ 6-submission-aplikasi-story-app/StoryApp/app/src/main/java/com/ervalsa/storyapp/data/model/User.kt
@@ -3,5 +3,6 @@ package com.ervalsa.storyapp.data.model
 data class User (
     var name: String,
     var email: String,
-    var password: String
+    var password: String,
+    var isLogin: Boolean
 )
---
+++ 6-submission-aplikasi-story-app/StoryApp/app/src/main/java/com/ervalsa/storyapp/views/CustomPasswordEditText.kt
@@ -0,0 +1,8 @@
+package com.ervalsa.storyapp.views
+
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+
+class CustomPasswordEditText {
+
+}
