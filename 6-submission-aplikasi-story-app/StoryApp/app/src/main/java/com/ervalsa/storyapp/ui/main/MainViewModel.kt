7e9c68302b8581e3f63524cab81110121d5dd9af
ervalsa <ervalsa-san@gmail.com>
2022-10-24 13:12:23 +0800

    Delete function storylist in mainviewmodel

--- 6-submission-aplikasi-story-app/StoryApp/app/src/main/java/com/ervalsa/storyapp/ui/main/MainViewModel.kt
+++ 6-submission-aplikasi-story-app/StoryApp/app/src/main/java/com/ervalsa/storyapp/ui/main/MainViewModel.kt
@@ -15,8 +15,6 @@ import retrofit2.Response
 
 class MainViewModel(private val preference: UserPreference) : ViewModel() {
 
-    val listStory = MutableLiveData<List<StoryItem>>()
-
     fun getUser(): LiveData<UserEntity> =
         preference.getUser().asLiveData()
 
@@ -24,26 +22,5 @@ class MainViewModel(private val preference: UserPreference) : ViewModel() {
         viewModelScope.launch {
             preference.logout()
         }
-    }
-
-    fun getAllStories() : LiveData<List<StoryItem>> = listStory
-
-    fun setListStory(token: String, page: Int? = null, size: Int? = null) {
-        val client = ApiConfig.getApiService().getAllStory(token, page, size)
-        client.enqueue(object : Callback<StoryResponse> {
-            override fun onResponse(call: Call<StoryResponse>, response: Response<StoryResponse>) {
-                val responseBody = response.body()
-                if (response.isSuccessful && responseBody != null) {
-                    listStory.postValue(responseBody.listStory)
-                    Log.e(TAG, responseBody.message)
-                } else {
-                    Log.e(TAG, "onFailure: ${response.message()}")
-                }
-            }
-
-            override fun onFailure(call: Call<StoryResponse>, t: Throwable) {
-                Log.e(TAG, "onFailure : ${t.message}")
-            }
-        })
     }
 }
