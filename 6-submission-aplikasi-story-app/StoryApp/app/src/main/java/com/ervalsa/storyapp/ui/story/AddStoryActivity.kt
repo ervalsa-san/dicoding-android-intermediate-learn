28cd8922fb49081745c69647932c92652317f03a
ervalsa <ervalsa-san@gmail.com>
2022-10-26 03:10:33 +0800

    Add intent CameraX and Gallery

--- 6-submission-aplikasi-story-app/StoryApp/app/src/main/java/com/ervalsa/storyapp/ui/story/AddStoryActivity.kt
+++ 6-submission-aplikasi-story-app/StoryApp/app/src/main/java/com/ervalsa/storyapp/ui/story/AddStoryActivity.kt
@@ -1,13 +1,30 @@
 package com.ervalsa.storyapp.ui.story
 
+import android.Manifest
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent.ACTION_GET_CONTENT
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
-import com.ervalsa.storyapp.R
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.ervalsa.storyapp.databinding.ActivityAddStoryBinding
-import com.ervalsa.storyapp.ui.main.MainActivity
+import com.ervalsa.storyapp.utils.rotateBitmap
+import com.ervalsa.storyapp.utils.uriToFile
+import java.io.File
 
 class AddStoryActivity : AppCompatActivity() {
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
+
+        private val REQUIRED_PERMISSION = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSION = 10
+    }
 
     private lateinit var binding: ActivityAddStoryBinding
 
@@ -20,5 +37,75 @@ class AddStoryActivity : AppCompatActivity() {
         val actionBar = supportActionBar
         actionBar?.title = ""
         actionBar?.elevation = 0f
+
+        if (!allPermissionGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSION,
+                REQUEST_CODE_PERMISSION
+            )
+        }
+
+        binding.btnCamera.setOnClickListener {
+            val intent = Intent(this@AddStoryActivity, CameraActivity::class.java)
+            launcherIntentCameraX.launch(intent)
+        }
+
+        binding.btnGallery.setOnClickListener {
+            val intent = Intent()
+            intent.action = ACTION_GET_CONTENT
+            intent.type = "image/"
+            val chooser = Intent.createChooser(intent, "Pilih gambar")
+            launcherIntentGallery.launch(chooser)
+        }
     }
+
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+       if (it.resultCode == CAMERA_X_RESULT) {
+           val myFile = it.data?.getSerializableExtra("picture") as File
+           val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+
+           val result = rotateBitmap(
+               BitmapFactory.decodeFile(myFile.path),
+               isBackCamera
+           )
+           binding.imgPhotoStory.setImageBitmap(result)
+       }
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg: Uri = result.data?.data as Uri
+            val myFile = uriToFile(selectedImg, this)
+            binding.imgPhotoStory.setImageURI(selectedImg)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            if (!allPermissionGranted()) {
+                Toast.makeText(
+                    this,
+                    "Tidak mendapatkan izin",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionGranted() = REQUIRED_PERMISSION.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
 }
---
+++ 6-submission-aplikasi-story-app/StoryApp/app/src/main/java/com/ervalsa/storyapp/ui/story/CameraActivity.kt
@@ -0,0 +1,110 @@
+package com.ervalsa.storyapp.ui.story
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.ervalsa.storyapp.databinding.ActivityCameraBinding
+import com.ervalsa.storyapp.utils.createFile
+
+class CameraActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCameraBinding
+
+    private var imageCapture: ImageCapture? = null
+    private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityCameraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        startCamera()
+
+        binding.switchCamera.setOnClickListener {
+            cameraSelector =
+                if (cameraSelector.equals(CameraSelector.DEFAULT_BACK_CAMERA))
+                    CameraSelector.DEFAULT_FRONT_CAMERA
+                else CameraSelector.DEFAULT_BACK_CAMERA
+
+            startCamera()
+        }
+
+        binding.captureImage.setOnClickListener {
+            takePhoto()
+        }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    preview,
+                    imageCapture
+                )
+            } catch (exception: Exception) {
+                Toast.makeText(
+                    this@CameraActivity,
+                    "Gagal memuat kamera.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+
+        val photoFile = createFile(application)
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val intent = Intent()
+                    intent.putExtra("picture", photoFile)
+                    intent.putExtra(
+                        "isBackCamera",
+                        cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA
+                    )
+                    setResult(AddStoryActivity.CAMERA_X_RESULT, intent)
+                    finish()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Toast.makeText(
+                        this@CameraActivity,
+                        "Gagal mengambil gambar.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+            }
+        )
+    }
+}
